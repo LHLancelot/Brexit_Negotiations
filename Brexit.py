e980ac913066c541c487dcff185e3effda673408
Leo Stockford <42842027+LHLancelot@users.noreply.github.com>
2022-02-04 17:04:07 +0200

    Add files via upload

---
+++ Brexit.py
@@ -0,0 +1,10 @@
+# A simple formula for explaining how many Brexit negotiations the United Kingdom will have
+
+def Calculate_negotiations():
+          i = 0
+          while True:
+              yield i
+              i += 1
+
+for x in Calculate_negotiations():
+    print("We're on negotiation number ", x)
