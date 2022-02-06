c22e76ff45b6414c9c001f1513320d17b1eb660e
Leo Stockford <42842027+LHLancelot@users.noreply.github.com>
2022-02-06 13:32:09 +0200

    Update Brexit.py

--- Brexit.py
+++ Brexit.py
@@ -1,9 +1,9 @@
-# A simple formula to forecast how many Brexit negotiations the United Kingdom will have
+# A simple formula to forecast how many days of Brexit negotiations the United Kingdom will have
 
 def Calculate_negotiations():
     x = 1
     while x>0:
-    print("We're on negotiation number ", x)
+    print("We're on negotiation day number ", x)
     x += 1
 
 Calculate_negotiations()
